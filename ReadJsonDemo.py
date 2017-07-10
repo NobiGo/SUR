5869259a815fa06ac80e9bb47d59c92ad584b278
dx <nobigogle@gmail.com>
2017-07-10 12:37:54 +0800

    Demo Succeed
    可以成功读取Jaon文件内容

--- ReadJsonDemo.py
+++ ReadJsonDemo.py
@@ -1,17 +1,20 @@
 # coding=utf-8
 import json
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import chardet
 
 # 解决不能读取中文文件名问题
 fileName = "高七七的病历.json"
-filePath = unicode(fileName,"utf8")
+filePath = fileName.decode("UTF-8")
 fileContent = open(filePath,"r")
 patientInfo = fileContent.read()
+# 显示文件的编码格式
+print chardet.detect(patientInfo)
 # 关闭文件
 fileContent.close()
 print patientInfo
 # 读取json文件
 content = json.loads(patientInfo,encoding="utf-8")
-print content['姓名']
+
+#  print chardet.detect("姓名")
+print content["姓名".decode("utf-8")]
+print content["出生地".decode("utf-8")]
